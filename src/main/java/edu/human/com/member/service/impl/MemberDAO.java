ee6c6c9d76f78dc78fc46486d11bc407553d72f6
kick3 <kick3@DESKTOP-HT6PHOD>
2021-02-15 17:41:02 +0900

    8교시 커밋

--- src/main/java/edu/human/com/member/service/impl/MemberDAO.java
+++ src/main/java/edu/human/com/member/service/impl/MemberDAO.java
@@ -17,7 +17,24 @@ import edu.human.com.member.service.EmployerInfoVO;
 public class MemberDAO extends EgovComAbstractMapper {
 	
 	public List<EmployerInfoVO> selectMember() throws Exception {
-		
 		return selectList("memberMapper.selectMember");
+	}
+	
+	public EmployerInfoVO viewMember(String emplyr_id) throws Exception {
+		//selctOne("쿼리ID","쿼리매개변수=파라미터=인자");
+		return selectOne("memberMapper.viewMember", emplyr_id);
+	}
+	
+	public int deleteMember(String emplyr_id) throws Exception {
+		//affected된 row값이 반환됩니다.
+		return delete("memberMapper.deleteMember", emplyr_id);
+	}
+	
+	public void insertMember(EmployerInfoVO employerInfoVO) throws Exception {
+		insert("memberMapper.insertMember", employerInfoVO);
+	}
+	
+	public void updateMember(EmployerInfoVO employerInfoVO) throws Exception {
+		update("memberMapper.updateMember", employerInfoVO);
 	}
 }
